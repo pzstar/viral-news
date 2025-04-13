c7a7c6ccafeedf8e9a0bee1493942ae97fa6ffde
prabin <prabin@macbook-pro.domain.name>
2025-04-13 17:37:02 +0545

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -61,7 +61,7 @@ if (!function_exists('viral_news_entry_footer')):
         if ('post' === get_post_type()) {
             /* translators: used between list items, there is a space after the comma */
             $categories_list = get_the_category_list(', ');
-            if ($categories_list && viral_news_categorized_blog()) {
+            if ($categories_list) {
                 printf('<div class="cat-links"><i class="mdi-book-open-outline"></i> ' . esc_html__('Posted in %1$s', 'viral-news') . '</div>', $categories_list); // WPCS: XSS OK.
             }
 
@@ -85,43 +85,13 @@ if (!function_exists('viral_news_entry_category')):
         if ('post' == get_post_type()) {
             /* translators: used between list items, there is a space after the comma */
             $categories_list = get_the_category_list(', ');
-            if ($categories_list && viral_news_categorized_blog()) {
+            if ($categories_list) {
                 echo '<i class="mdi-book-open-outline"></i> ' . $categories_list; // WPCS: XSS OK.
             }
         }
     }
 
 endif;
-
-/**
- * Returns true if a blog has more than 1 category.
- *
- * @return bool
- */
-function viral_news_categorized_blog() {
-    if (false === ($all_the_cool_cats = get_transient('viral_news_categories'))) {
-        // Create an array of all the categories that are attached to posts.
-        $all_the_cool_cats = get_categories(array(
-            'fields' => 'ids',
-            'hide_empty' => 1,
-            // We only need to know if there is more than one category.
-            'number' => 2,
-        ));
-
-        // Count the number of categories that are attached to the posts.
-        $all_the_cool_cats = count($all_the_cool_cats);
-
-        set_transient('viral_news_categories', $all_the_cool_cats);
-    }
-
-    if ($all_the_cool_cats > 1) {
-        // This blog has more than 1 category so viral_news_categorized_blog should return true.
-        return true;
-    } else {
-        // This blog has only 1 category so viral_news_categorized_blog should return false.
-        return false;
-    }
-}
 
 if (!function_exists('viral_news_post_primary_category')) {
 
@@ -177,20 +147,6 @@ if (!function_exists('viral_news_get_post_primary_category')) {
     }
 
 }
-
-/**
- * Flush out the transients used in viral_news_categorized_blog.
- */
-function viral_news_category_transient_flusher() {
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-        return;
-    }
-    // Like, beat it. Dig?
-    delete_transient('viral_news_categories');
-}
-
-add_action('edit_category', 'viral_news_category_transient_flusher');
-add_action('save_post', 'viral_news_category_transient_flusher');
 
 
 /**
